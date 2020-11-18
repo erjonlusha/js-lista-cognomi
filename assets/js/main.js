972eb1d1409f0d5b66f31b25f734bb1df695565f
Erjon <erjonlusha@gmail.com>
2020-11-18 17:46:07 +0100

    Primo commit
    Lista Cognomi
    Chiedere all'utente il cognome
    Inserirlo in un array con altri cognomi: ‘Bianchi', ‘Rossi', ‘Duzioni', ‘Balsano', ‘Verdi'
    Stampa la lista ordinata alfabeticamente
    Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Lista Cognomi
+// Chiedere all'utente il cognome
+// Inserirlo in un array con altri cognomi: ‘Bianchi', ‘Rossi', ‘Duzioni', ‘Balsano', ‘Verdi'
+// Stampa la lista ordinata alfabeticamente
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var cognomeUser = prompt("Inserisci cognome");
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+cognomi.push(cognomeUser);
+cognomi.sort();
+
+for (var i = 0, c=0; i < cognomi.length; i++, c++){
+    var persone = cognomi[i];
+    var ordAlfabetico =document.getElementById("ordine_alfabetico").innerHTML;
+    document.getElementById("ordine_alfabetico").innerHTML = ordAlfabetico + "<li>" + persone + "</li>";
+    if (cognomeUser == cognomi) {
+        console.log(cognomi[i]);
+        console.log(c);
+        break
+    }
+}
