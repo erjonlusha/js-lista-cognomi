afbb9465cd301ae18f471dbe29a1e91934dcf846
Erjon <erjonlusha@gmail.com>
2020-11-19 11:25:38 +0100

    Scrivi anche la posizione
    "umana" della lista in cui il nuovo utente si trova

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,16 @@ var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 cognomi.push(cognomeUser);
 cognomi.sort();
 
-for (var i = 0, c=0; i < cognomi.length; i++, c++){
-    var persone = cognomi[i];
-    var ordAlfabetico =document.getElementById("ordine_alfabetico").innerHTML;
-    document.getElementById("ordine_alfabetico").innerHTML = ordAlfabetico + "<li>" + persone + "</li>";
-    if (cognomeUser == cognomi) {
-        console.log(cognomi[i]);
-        console.log(c);
-        break
-    }
-}
+// for (var i = 0, c=0; i < cognomi.length; i++, c++){
+//     var persone = cognomi[i];
+//     var ordAlfabetico =document.getElementById("ordine_alfabetico").innerHTML;
+//     document.getElementById("ordine_alfabetico").innerHTML = ordAlfabetico + "<li>" + persone + "</li>";
+//     if (cognomeUser == cognomi) {
+//         console.log(cognomi[i]);
+//         console.log(c);
+//         break
+//     }
+// }
+
+var positionCognomeUser = cognomi.indexOf(cognomeUser) + 1;
+console.log(positionCognomeUser);
